491b96f8b7b8a5cbe4f9221c20b738bc299d9f1f
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-04-13 17:14:42 +0700

    add login skeleton

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 #App
-  b-navbar
+  b-navbar(shadow)
     template(slot="brand")
       b-navbar-item(tag="router-link" to="/")
         strong Media
@@ -9,11 +9,70 @@
       b-navbar-item(tag="router-link" to="/play") Play
       b-navbar-item(tag="router-link" to="/edit") Edit
       b-navbar-item(tag="router-link" to="/browse") Browse
-      b-navbar-item(href="https://github.com/patarapolw" target="_blank") GitHub
+      b-navbar-item(href="https://github.com/patarapolw/mediasync" target="_blank") GitHub
     template(slot="end")
       b-navbar-item(tag="div" v-if="user") Signed in as {{user.email}}
       b-navbar-item(tag="div")
         .buttons
           b-button(v-if="user" type="is-warning" @click="doLogout") Logout
-          b-button(v-else type="is-primary" @click="doLogin") Login
+          b-button(v-else type="is-primary" @click="isLoginModal = true") Login
+  main
+    router-view
+  b-modal(:active.sync="isLoginModal" :can-cancel="false")
+    div(ref="auth")
 </template>
+
+<script lang="ts">
+import { Vue, Component, Watch } from 'vue-property-decorator'
+import firebase from 'firebase/app'
+import { auth as authUI } from 'firebaseui'
+
+import 'firebase/auth'
+import 'firebaseui/dist/firebaseui.css'
+
+let firebaseUI: authUI.AuthUI | null
+
+@Component
+export default class App extends Vue {
+isDrawer = false
+  isLoginModal = false
+
+  get user () {
+    return this.$store.state.user
+  }
+
+  get isAuthenticated () {
+    return this.$store.state.lastStatus !== 401
+  }
+
+  @Watch('isLoginModal')
+  onLogin () {
+    this.$nextTick(() => {
+      if (this.$refs.auth) {
+        if (!firebaseUI) {
+          firebaseUI = new authUI.AuthUI(firebase.auth())
+        }
+
+        firebaseUI.start(this.$refs.auth as HTMLDivElement, {
+          signInSuccessUrl: this.$router.resolve('/').href,
+          signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+          ],
+          signInFlow: 'popup'
+        })
+      }
+    })
+  }
+
+  @Watch('isAuthenticated')
+  onLogout () {
+    if (!this.isAuthenticated) {
+      this.isLoginModal = true
+    }
+  }
+
+  doLogout () {
+    firebase.auth().signOut()
+  }
+}
+</script>
--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,10 +1,43 @@
 import Vue from 'vue'
+import dayjs from 'dayjs'
+import firebase from 'firebase/app'
+
+import 'firebase/analytics'
+import 'firebase/auth'
+
 import App from './App.vue'
 import router from './router'
+import store from './store'
+
+import './plugins/buefy'
+import './plugins/fontawesome'
+import './plugins/codemirror'
+
+firebase.initializeApp(require('../firebase.config'))
 
 Vue.config.productionTip = false
 
+Vue.filter('format', (v: any) => {
+  if (typeof v === 'number') {
+    return (v || v === 0) ? v.toLocaleString() : ''
+  } else if (v instanceof Date) {
+    return dayjs(v).format('YYYY-MM-DD HH:mm')
+  } else if (v && typeof v === 'object') {
+    return JSON.stringify(v)
+  }
+  return v
+})
+
+Vue.filter('formatDate', (v: any) => {
+  return dayjs(v).format('YYYY-MM-DD HH:mm')
+})
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
+
+firebase.auth().onAuthStateChanged((user) => {
+  store.commit('setUser', user)
+})
